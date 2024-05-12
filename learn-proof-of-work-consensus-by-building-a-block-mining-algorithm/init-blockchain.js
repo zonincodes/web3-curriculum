d6548733e717c5d3613d69e1fde88f9aeb18ea86
zonin <business.kariukimartin@gmail.com>
2024-05-12 01:12:00 -0400

    fix:

--- learn-proof-of-work-consensus-by-building-a-block-mining-algorithm/init-blockchain.js
+++
@@ -1,9 +0,0 @@
-import { writeBlockchain } from './blockchain-helpers.js';
-
-const genesisBlock = {
-  hash: "0",
-  previousHash: null
-}
-
-const blockchain = [genesisBlock];
-writeBlockchain(blockchain);
